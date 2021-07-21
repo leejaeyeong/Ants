c3bc0092362fc65f39313abe9ac21879c68b381b
황지훈 <01052405583a@gmail.com>
2021-07-21 14:34:20 +0900

    feat: 설정 수정

--- frontend/src/store/module/actions.js
+++ frontend/src/store/module/actions.js
@@ -4,7 +4,7 @@ import $axios from 'axios'
 // }
 export function login ({ state }, payload) {
   console.log('requestLogin', state, payload)
-  const url = '/auth/login'
+  const url = '/api/v1/auth/login'
   const body = payload
   return $axios.post(url, body)
 }
