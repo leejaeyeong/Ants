f1b24f5d5a73eda9b0d93a6482f1fb313d952c8e
DESKTOP-KVCQHCD\multicampus <last9500@naver.com>
2021-07-21 14:49:04 +0900

    test

--- frontend/src/store/module/actions.js
+++ frontend/src/store/module/actions.js
@@ -4,7 +4,7 @@ import $axios from 'axios'
 // }
 export function login ({ state }, payload) {
   console.log('requestLogin', state, payload)
-  const url = '/auth/login'
+  const url = 'http://localhost:8080/api/v1/auth/login'
   const body = payload
   return $axios.post(url, body)
 }
