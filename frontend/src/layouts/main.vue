8c801a10e917628a8a80e96d5a4976d69ae9eef3
황지훈 <01052405583a@gmail.com>
2021-07-23 14:35:12 +0900

    feat: 사이드바 ui 재구성 / router 기능 구현

--- frontend/src/layouts/main.vue
+++ frontend/src/layouts/main.vue
@@ -1,26 +1,7 @@
 <template>
-    <div id="header">
-        <div id="headerLeft">
-            <q-img src="~assets/logo.png" style="width:150px; margin-top:-43px;"/>
-        </div>
-        <div id="headerRight">
-            <q-img src="~assets/user.png" style="width:40px; margin-top:10px; margin-right:45px; float:right;"/>
-            <q-img src="~assets/bell.png" style="width:30px; margin-top:15px; margin-right:5px; float:right;"/>
-            <span style="font-weight:bold; float:right; margin-right:8px; line-height:60px; font-size:15px;">{{ state.id }} 님</span>
-        </div>
-    </div>
+    <Header></Header>
     <div id="content">
-        <div id="side">
-            <q-item  @click="menuSelect(link)" v-for="link in links1" :key="link.text" v-ripple clickable>
-                <q-item-section avatar>
-                    <q-icon color="grey" :name="link.icon" />
-                </q-item-section>
-                <q-item-section>
-                    <q-item-label>{{ link.text }}</q-item-label>
-                </q-item-section>
-            </q-item>
-            <q-btn @click="logout" id="logoutbtn" align="between" class="btn-fixed-width" color="accent" label="로그아웃" icon="flight_takeoff" />
-        </div>
+        <Side></Side>
         <div id="main">
             <div id="topLeft">
             </div>
@@ -35,84 +16,26 @@
 </template>
 
 <script>
-import { reactive, defineComponent } from 'vue'
-import { fabYoutube } from '@quasar/extras/fontawesome-v5'
-import { useRouter } from 'vue-router'
+import { defineComponent } from 'vue'
+import Header from '../components/header.vue'
+import Side from '../components/side.vue'
 
 export default defineComponent({
   name: 'login',
-  methods: {
-    menuSelect (link) {
-      this.$router.push('/' + link.name)
-    }
-  },
-  setup () {
-    const state = reactive({
-      id: localStorage.getItem('id')
-    })
-
-    const router = useRouter()
-    const logout = function () {
-      localStorage.removeItem('token')
-      localStorage.removeItem('id')
-      localStorage.removeItem('password')
-      localStorage.removeItem('name')
-      router.push('/')
-    }
-
-    return {
-      state,
-      fabYoutube,
-      logout,
-      links1: [
-        { icon: 'home', text: '홈', name: 'main' },
-        { icon: 'flag', text: '공지사항', name: 'notice' },
-        { icon: 'feedback', text: '익명게시판', name: 'board' },
-        { icon: 'subscriptions', text: '회의', name: 'rtc' },
-        { icon: 'settings', text: '설정', name: 'setting' }
-      ]
-    }
+  components: {
+    Header,
+    Side
   }
 })
 </script>
 
 <style scoped>
-#header{
-    width:100%;
-    height:60px;
-    border-bottom:0.5px solid rgb(212, 212, 212);
-}
-#headerLeft{
-    width:12%;
-    height:60px;
-    float:left;
-    text-align: center;
-    font-size:35px;
-    /* border-right:0.5px solid rgb(212, 212, 212); */
-}
-#headerRight{
-    width:88%;
-    height:60px;
-    float:right;
-}
 #content{
     width:100%;
     height: 680px;
 }
-#side{
-    width:12%;
-    height:692px;
-    float:left;
-    border-right:0.5px solid rgb(212, 212, 212);
-    border-bottom:0.5px solid rgb(212, 212, 212);
-}
-#logoutbtn{
-    position: fixed;
-    top:93%;
-    left:2%;
-}
 #main{
-    width:88%;
+    width:95%;
     height:692px;
     float:right;
     border-bottom:0.5px solid rgb(212, 212, 212);
