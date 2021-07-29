c273be1106ab60b864a9550da4bfa3e023181edc
정범진 <last9500@naver.com>
2021-07-29 13:42:26 +0900

    add : rtc vue 추가

---
+++ frontend/src/layouts/rtcTest.vue
@@ -0,0 +1,43 @@
+<template>
+    <Header></Header>
+    <div>
+        <Side></Side>
+        <div id="container">
+            <div id="wrapper">
+                <div id="join" class="animate join">
+                    <h1>Join a Room</h1>
+                    <form onsubmit="register(); return false;" accept-charset="UTF-8">
+                        <p>
+                            <input type="text" name="name" value="" id="name" placeholder="Username" required>
+                        </p>
+                        <p>
+                            <input type="text" name="room" value="" id="roomName" placeholder="Room" required>
+                        </p>
+                        <p class="submit"><input type="submit" name="commit" value="Join!">
+                        </p>
+                    </form>
+                </div>
+                <div id="room" style="display: none;">
+                    <h2 id="room-header"></h2>
+                    <div id="participants"></div>
+                    <input type="button" id="button-leave" onmouseup="leaveRoom();" value="Leave room">
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import { defineComponent } from 'vue'
+import Header from '../components/header.vue'
+import Side from '../components/side.vue'
+
+export default defineComponent({
+  setup () {},
+  components: {
+    Header,
+    Side
+  }
+})
+</script>
+<style>
+</style>
