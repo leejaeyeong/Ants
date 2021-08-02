909fb3ca65a2bd73524a2e09645885b0a01bfcd3
황지훈 <01052405583a@gmail.com>
2021-08-02 10:46:49 +0900

    Revert "feat: rtc화면 초기 틀 구현"
    This reverts commit 451ea63e7e0ee65c46818e6c36a9e164712d8d87.

--- frontend/src/layouts/rtcTest.vue
+++ frontend/src/layouts/rtcTest.vue
@@ -5,23 +5,22 @@
         <div id="container">
             <div id="wrapper">
                 <div id="join" class="animate join">
-                    <!-- <h1>Join a Room</h1> -->
+                    <h1>Join a Room</h1>
                     <form onsubmit="register(); return false;" accept-charset="UTF-8">
                         <p>
-                            <input style="margin-top:10px;" type="text" name="room" value="" id="roomName" placeholder="Room" required>
+                            <input type="text" name="name" value="" id="name" placeholder="Username" required>
                         </p>
                         <p>
-                            <input :model="name" style="display:none;" type="text" name="name" value="" id="name" placeholder="Username" required>
+                            <input type="text" name="room" value="" id="roomName" placeholder="Room" required>
                         </p>
-                        <p class="submit"><input id="submit" type="submit" name="commit" value="회의생성">
+                        <p class="submit"><input type="submit" name="commit" value="Join!">
                         </p>
                     </form>
                 </div>
                 <div id="room" style="display: none;">
-                    <!-- <h2 id="room-header"></h2> -->
+                    <h2 id="room-header"></h2>
                     <div id="participants"></div>
-                    <div id="chat"></div>
-                    <input type="button" id="button-leave" onmouseup="leaveRoom();" value="나가기">
+                    <input type="button" id="button-leave" onmouseup="leaveRoom();" value="Leave room">
                 </div>
             </div>
         </div>
@@ -37,47 +36,8 @@ export default defineComponent({
   components: {
     Header,
     Side
-  },
-  data () {
-    return {
-      name: localStorage.getItem('name')
-    }
   }
 })
 </script>
 <style>
-#submit{
-    background-color:#18C75E;
-    font-weight:bold;
-    color:white;
-    padding:10px;
-    font-size:20px;
-    cursor: pointer;
-    border:0.5px solid #18C75E;
-    border-radius:5px;
-    position:absolute;
-    top:690px;
-    left:1370px;
-}
-#submit:hover{
-    opacity: 0.7;
-}
-#participants{
-    width:1150px;
-    height:690px;
-    border:1px solid green;
-    position:absolute;
-    left:75px;
-}
-#chat{
-    width:295px;
-    height:690px;
-    border:1px solid blue;
-    position:absolute;
-    left:1225px;
-}
-#button-leave{
-    position:fixed;
-    top:95%;
-}
 </style>
