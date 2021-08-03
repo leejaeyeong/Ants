e95a752c4a678b5a0bde65fad1079d4b17135dee
이재영 <giver12@naver.com>
2021-08-03 22:37:09 +0900

    feat: 부서 목록 API 구현

--- frontend/src/store/module/getters.js
+++ frontend/src/store/module/getters.js
@@ -4,3 +4,7 @@ export function someGetter (/* state */) {
 export function getLoginUser (state) {
   return state.loginUser
 }
+
+export function getDepartmentInfo (state) {
+  return state.department
+}
