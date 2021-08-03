8ae6ec93fbc0cb985993fe368aa0700a10af29d5
이재영 <giver12@naver.com>
2021-08-03 22:36:26 +0900

    feat: 유저 프로필 이미지 등록 기능

--- backend/src/main/java/com/ssafy/api/service/UserService.java
+++ backend/src/main/java/com/ssafy/api/service/UserService.java
@@ -14,7 +14,7 @@ import java.util.Map;
  *	유저 관련 비즈니스 로직 처리를 위한 서비스 인터페이스 정의.
  */
 public interface UserService {
-	User createUser(UserRegisterPostReq userRegisterInfo);
+	User createUser(UserRegisterPostReq userRegisterInfo) throws Exception;
 	User getUserByUserId(String userId);
 	boolean getTeamByUserId(String userId);
 	boolean updateUserTeamInfo(UserTeamMappingPutReq userTeamMappingPutReq);
