a63afd8fde9cb61b6658b76ceea6037142cebb34
김지안 <jiahn.dev@gmail.com>
2021-08-05 11:19:49 +0900

    fix: 이전달, 다음달 이동 수정

--- frontend/src/pages/user/MyManagement.vue
+++ frontend/src/pages/user/MyManagement.vue
@@ -1,193 +1,170 @@
 <template>
   <div id="myData">
     <div class="q-pa-md">
-      <q-markup-table separator="cell" flat bordered>
+      <section class="section">
+        <p class="subtitle has-text-centered">
+          <button class="button is-small is-primary is-outlined mr-5"
+          @click="calendarData(-1)">&lt;</button>
+          {{ year }}년 {{ month }}월
+          <button class="button is-small is-primary is-outlined ml-5"
+          @click="calendarData(1)">&gt;</button>
+        </p>
+        <q-markup-table separator="cell" flat bordered class="table has-text-centered is-fullwidth">
           <thead>
-            <tr>
-              <th colspan="7">
-                <div class="row no-wrap items-center">
-                  <div class="year">{{ viewYear }}</div>
-                  <br>
-                  <div class="month">{{ viewMonth }}</div>
-                  <br>
-                  <div class="nav">
-                      <button class="nav-btn go-prev" @click="prevMonth">&lt;</button>
-                      <button class="nav-btn go-today">Today</button>
-                      <button class="nav-btn go-next" @click="nextMonth">&gt;</button>
-                  </div>
-                </div>
-              </th>
-            </tr>
-            <tr>
-              <th class="text-right">Sun.</th>
-              <th class="text-right">Mon.</th>
-              <th class="text-right">Tue.</th>
-              <th class="text-right">Wed.</th>
-              <th class="text-right">Thu</th>
-              <th class="text-right">Fri.</th>
-              <th class="text-right">Sat.</th>
-            </tr>
+            <th v-for="day in days" :key="day">{{ day }}</th>
           </thead>
-        <tbody>
-          <tr v-for="(date, idx) in renderCalendar()" :key="idx" >
-            <td
-              v-for="(day, secondIdx) in date" :key="secondIdx" class="q-td--no-hover" style="height: 110px; width:100px;" @click="clickday(day)"
-            >
-              <span v-if="(idx > 3 && day < 6) || (idx <2 && day >25)" class="other">{{ day }}</span>
-              <span v-else-if="(secondIdx === 0)" style="color: red;">{{ day }}</span>
-              <span v-else-if="(secondIdx === 6)" style="color: blue;">{{ day }}</span>
-              <span v-else-if="(Today === day)" class="today">{{ day }}</span>
-              <span v-else>{{ day }}</span>
-            </td>
-          </tr>
-        </tbody>
-      </q-markup-table>
+          <tbody>
+            <tr v-for="(date, idx) in dates" :key="idx">
+              <td
+                v-for="(day, secondIdx) in date"
+                :key="secondIdx"
+                class="q-td--no-hover"
+                style="height: 140px; width:130px;"
+              >
+                <span v-if="(idx > 3 && day < 6) || (idx <2 && day >25)" class="other">{{ day }}</span>
+                <span v-else-if="(secondIdx === 0)" style="color: red;">{{ day }}</span>
+                <span v-else-if="(secondIdx === 6)" style="color: blue;">{{ day }}</span>
+                <span v-else-if="(today === day && month === currentMonth)" class="today">{{ day }}</span>
+                <span v-else>{{ day }}</span>
+              </td>
+            </tr>
+          </tbody>
+        </q-markup-table>
+      </section>
     </div>
   </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
-
-export default defineComponent({
+import axios from 'axios'
+export default {
   name: 'management',
-  setup () {
-    const date = new Date()
-    const viewYear = date.getFullYear()
-    const viewMonth = date.getMonth() + 1
-
-    // 달력 렌더링함수
-    const renderCalendar = function (month) {
-      const viewYear = date.getFullYear()
-      const viewMonth = date.getMonth()
-      // if(month === 0) {
-      //   viewMonth ==
-
-      // }
-      // year-month 채우기
-
-      // 지난 달 마지막 Date, 이번 달 마지막 Date
-      const prevLast = new Date(viewYear, viewMonth, 0)
-      const thisLast = new Date(viewYear, viewMonth + 1, 0)
-
-      const PLDate = prevLast.getDate()
-      const PLDay = prevLast.getDay()
-
-      const TLDate = thisLast.getDate()
-      const TLDay = thisLast.getDay()
-
-      // Dates 기본 배열들
-      const prevDates = []
-      const thisDates = [...Array(TLDate + 1).keys()].slice(1)
-      const nextDates = []
-
-      // prevDates 계산
-      if (PLDay !== 6) {
-        for (let i = 0; i < PLDay + 1; i++) {
-          prevDates.unshift(PLDate - i)
-        }
-      }
-
-      // nextDates 계산
-      for (let i = 1; i < 7 - TLDay; i++) {
-        nextDates.push(i)
-      }
-
-      // 저번달, 이번달, 다음달 날짜 합치기
-      const monthday = []
-      let weekday = []
-      const dates = prevDates.concat(thisDates, nextDates)
-      for (let i = 0; i < dates.length; i++) {
-        weekday.push(dates[i])
-        if (weekday.length === 7) {
-          monthday.push(weekday)
-          weekday = []
-        }
-      }
-      console.log(monthday, '함수안에서monthday')
-      return monthday
-    }
-    // const resultMonth = renderCalendar()
-    // console.log(resultMonth, 'resultmonth')
-
-    const prevMonth = function () {
-      date.setMonth(date.getMonth() - 1)
-      console.log('prev')
-      renderCalendar()
-    }
-
-    const nextMonth = function () {
-      date.setMonth(date.getMonth() + 1)
-      renderCalendar()
-    }
-    // 오늘날짜 표시해주기
-    const Today = date.getDate()
-    console.log(Today)
-    // // // 날짜가져옴
-    // const timeStamp = Date.now()
-    // const viewYear = date.formatDate(timeStamp, 'YYYY')
-    // const viewMonth = date.formatDate(timeStamp, 'MM')
-    // console.log(viewYear, viewMonth)
-
-    // // 지난달
-    // const prevLast = new Date(viewYear, viewMonth - 1, 0)
-    // // 마지막날짜
-    // const PLDate = prevLast.getDate()
-    // // 요일
-    // const PLDay = prevLast.getDay()
-    // console.log('지난달', prevLast, PLDate, PLDay)
-    // // 이번달
-    // const thisLast = new Date(viewYear, viewMonth, 0)
-    // const TLDate = thisLast.getDate()
-    // const TLDay = thisLast.getDay()
-    // console.log('이번달', thisLast, TLDate, TLDay)
-
-    // const prevDates = []
-    // const thisDates = [...Array(TLDate + 1).keys()].slice(1)
-    // const nextDates = []
-    // console.log(thisDates)
-    // if (PLDay !== 6) {
-    //   for (let i = 0; i < PLDay + 1; i++) {
-    //     prevDates.unshift(PLDate - i)
-    //   }
-    // }
-
-    // for (let i = 1; i < 7 - TLDay; i++) {
-    //   nextDates.push(i)
-    // }
-    // // 저번달, 이번달, 다음달 날짜 합치기
-    // const monthday = []
-    // let weekday = []
-    // const day = 0
-    // const dates = prevDates.concat(thisDates, nextDates)
-    // console.log(dates)
-    // console.log(monthday, weekday, day, dates, '96라인')
-    // console.log(dates.length)
-    // for (let i = 0; i < dates.length; i++) {
-    //   weekday.push(dates[i])
-    //   if (weekday.length === 7) {
-    //     monthday.push(weekday)
-    //     weekday = []
-    //   }
-    // }
-
-    // 날짜 클릭이벤트 만들기 => 매일 출퇴근시간 받아오기, 투두리스트 crud
-    // 모달창?
-    const clickday = function (day) {
-      alert(day, '클릭함')
-    }
-    // 스타일 정리하기
+  data () {
     return {
-      viewYear,
-      viewMonth,
-      Today,
-      prevMonth,
-      nextMonth,
-      clickday,
-      renderCalendar
+      days: [
+        'Sun',
+        'Mon',
+        'Tue',
+        'Wed',
+        'Thu',
+        'Fri',
+        'Sat'
+      ],
+      dates: [],
+      currentYear: 0,
+      currentMonth: 0,
+      year: 0,
+      month: 0,
+      lastMonthStart: 0,
+      nextMonthStart: 0,
+      today: 0
+    }
+  },
+  created () { // 데이터에 접근이 가능한 첫 번째 라이프 사이클
+    const date = new Date()
+    this.currentYear = date.getFullYear() // 이하 현재 년, 월 가지고 있기
+    this.currentMonth = date.getMonth() + 1
+    this.year = this.currentYear
+    this.month = this.currentMonth
+    this.today = date.getDate() // 오늘 날짜
+    this.getWork(this.year, this.month)
+  },
+  methods: {
+    calendarData (arg) { // 인자를 추가
+      if (arg < 0) { // -1이 들어오면 지난 달 달력으로 이동
+        this.month -= 1
+      } else if (arg === 1) { // 1이 들어오면 다음 달 달력으로 이동
+        this.month += 1
+      }
+      if (this.month === 0) { // 작년 12월
+        this.year -= 1
+        this.month = 12
+      } else if (this.month > 12) { // 내년 1월
+        this.year += 1
+        this.month = 1
+      }
+      const [
+        monthFirstDay,
+        monthLastDate,
+        lastMonthLastDate
+      ] = this.getFirstDayLastDate(this.year, this.month)
+      this.dates = this.getMonthOfDays(
+        monthFirstDay,
+        monthLastDate,
+        lastMonthLastDate
+      )
+    },
+    getFirstDayLastDate (year, month) {
+      const firstDay = new Date(year, month - 1, 1).getDay() // 이번 달 시작 요일
+      const lastDate = new Date(year, month, 0).getDate() // 이번 달 마지막 날짜
+      let lastYear = year
+      let lastMonth = month - 1
+      if (month === 1) {
+        lastMonth = 12
+        lastYear -= 1
+      }
+      const prevLastDate = new Date(lastYear, lastMonth, 0).getDate() // 지난 달 마지막 날짜
+      return [firstDay, lastDate, prevLastDate]
+    },
+    getMonthOfDays (
+      monthFirstDay,
+      monthLastDate,
+      prevMonthLastDate
+    ) {
+      let day = 1
+      let prevDay = (prevMonthLastDate - monthFirstDay) + 1
+      const dates = []
+      let weekOfDays = []
+      while (day <= monthLastDate) {
+        if (day === 1) {
+          // 1일이 어느 요일인지에 따라 테이블에 그리기 위한 지난 셀의 날짜들을 구할 필요가 있다.
+          for (let j = 0; j < monthFirstDay; j += 1) {
+            if (j === 0) this.lastMonthStart = prevDay // 지난 달에서 제일 작은 날짜
+            weekOfDays.push(prevDay)
+            prevDay += 1
+          }
+        }
+        weekOfDays.push(day)
+        if (weekOfDays.length === 7) {
+          // 일주일 채우면
+          dates.push(weekOfDays)
+          weekOfDays = [] // 초기화
+        }
+        day += 1
+      }
+      const len = weekOfDays.length
+      if (len > 0 && len < 7) {
+        for (let k = 1; k <= 7 - len; k += 1) {
+          weekOfDays.push(k)
+        }
+      }
+      if (weekOfDays.length > 0) dates.push(weekOfDays) // 남은 날짜 추가
+      this.nextMonthStart = weekOfDays[0] // 이번 달 마지막 주에서 제일 작은 날짜
+      console.log(dates)
+      return dates
+    },
+    getWork (year, month) {
+      console.log(year, month)
+      if (String(month).length === 1) {
+        month = '0' + String(month)
+      }
+      console.log(year, month)
+      const baseUrl = 'https://localhost:8443/'
+      const id = localStorage.getItem('id')
+      const requestData = {
+        method: 'get',
+        url: baseUrl + 'api/v1/users/' + id + '/attendance/' + year + '/' + month
+      }
+      axios(requestData)
+        .then(function (response) {
+          console.log(response)
+        })
+        .catch(function (err) {
+          console.log(err)
+        })
     }
   }
-})
+}
 </script>
 
 <style scoped>
