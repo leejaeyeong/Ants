e69b4bba3f6352a196f73c1d22c225b2002f2caa
김지안 <jiahn.dev@gmail.com>
2021-08-05 14:05:51 +0900

    fix: header, sidebar 정리

--- frontend/src/layouts/rtcTest.vue
+++ frontend/src/layouts/rtcTest.vue
@@ -1,54 +1,50 @@
 <template>
-    <Header></Header>
-    <div>
-        <Side></Side>
-        <div id="container">
-            <div id="wrapper">
-                <div id="join" class="animate join">
-                    <!-- <h1>Join a Room</h1> -->
-                    <form onsubmit="register(); return false;" accept-charset="UTF-8">
-                        <p>
-                            <input v-show="false" v-model="state.roomName" style="margin-top:10px;" type="text" name="room" id="roomName" placeholder="Room" required>
-                        </p>
-                        <p>
-                            <input v-show="false" v-model="state.id" type="text" name="name" id="name" placeholder="Username" required>
-                        </p>
-                        <p class="submit"><input id="submit" type="submit" name="commit" value="회의생성">
-                        </p>
-                    </form>
-                    <div class="q-pa-md row items-start q-gutter-md">
-                      <q-card v-for="room in rooms" :key="room.id" class="my-card">
-                          <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
-                            <div class="absolute-bottom">
-                              <div class="text-h6">{{ room.roomName }}</div>
-                              <div class="text-subtitle2">{{ room.id }}</div>
-                            </div>
-                          </q-img>
-                          <q-card-actions>
-                            <!-- <q-btn class="enter">회의입장</q-btn> -->
-                            <input id="enter" type="submit" name="commit" value="회의입장">
-                          </q-card-actions>
-                      </q-card>
-                    </div>
-                </div>
-                <div id="room" style="display: none;">
-                    <!-- <h2 id="room-header"></h2> -->
-                    <div id="participants"></div>
-                    <div id="chat"></div>
-                    <input type="button" id="button-leave" onmouseup="leaveRoom();" value="나가기">
-                </div>
-            </div>
-        </div>
-        <div id="room" style="display: none">
-          <h2 id="room-header"></h2>
-          <div id="participants"></div>
-          <input
-            type="button"
-            id="button-leave"
-            onmouseup="leaveRoom();"
-            value="Leave room"
-          />
-        </div>
+  <div id="container">
+      <div id="wrapper">
+          <div id="join" class="animate join">
+              <!-- <h1>Join a Room</h1> -->
+              <form onsubmit="register(); return false;" accept-charset="UTF-8">
+                  <p>
+                      <input v-show="false" v-model="state.roomName" style="margin-top:10px;" type="text" name="room" id="roomName" placeholder="Room" required>
+                  </p>
+                  <p>
+                      <input v-show="false" v-model="state.id" type="text" name="name" id="name" placeholder="Username" required>
+                  </p>
+                  <p class="submit"><input id="submit" type="submit" name="commit" value="회의생성">
+                  </p>
+              </form>
+              <div class="q-pa-md row items-start q-gutter-md">
+                <q-card v-for="room in rooms" :key="room.id" class="my-card">
+                    <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
+                      <div class="absolute-bottom">
+                        <div class="text-h6">{{ room.roomName }}</div>
+                        <div class="text-subtitle2">{{ room.id }}</div>
+                      </div>
+                    </q-img>
+                    <q-card-actions>
+                      <!-- <q-btn class="enter">회의입장</q-btn> -->
+                      <input id="enter" type="submit" name="commit" value="회의입장">
+                    </q-card-actions>
+                </q-card>
+              </div>
+          </div>
+          <div id="room" style="display: none;">
+              <!-- <h2 id="room-header"></h2> -->
+              <div id="participants"></div>
+              <div id="chat"></div>
+              <input type="button" id="button-leave" onmouseup="leaveRoom();" value="나가기">
+          </div>
+      </div>
+  </div>
+  <div id="room" style="display: none">
+    <h2 id="room-header"></h2>
+    <div id="participants"></div>
+    <input
+      type="button"
+      id="button-leave"
+      onmouseup="leaveRoom();"
+      value="Leave room"
+    />
   </div>
   <div id="room" style="display: none">
     <h2 id="room-header"></h2>
@@ -64,8 +60,6 @@
 </template>
 <script>
 import { reactive, computed, defineComponent, onMounted } from 'vue'
-import Header from '../components/header.vue'
-import Side from '../components/side.vue'
 import { useStore } from 'vuex'
 
 export default defineComponent({
@@ -97,10 +91,6 @@ export default defineComponent({
       rooms,
       state
     }
-  },
-  components: {
-    Header,
-    Side
   }
 })
 </script>
