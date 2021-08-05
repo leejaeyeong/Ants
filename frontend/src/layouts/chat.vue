4dedbd305473fbf268e4071e4499d25d51b3fbae
DESKTOP-KVCQHCD\multicampus <last9500@naver.com>
2021-08-05 17:33:58 +0900

    feat: rtc 회의방 참가 기능 추가

--- frontend/src/layouts/chat.vue
+++ frontend/src/layouts/chat.vue
@@ -2,17 +2,21 @@
     <div id="main-content" class="container">
         <div class="row">
             <div class="col-md-6">
-                <form class="form-inline">
+                <form class="form-inline" onsubmit="connect(); return false;" accept-charset="UTF-8">
                     <div class="form-group">
                         <label for="connect">WebSocket connection:</label>
                         <button id="connect" class="btn btn-default" type="submit">Connect</button>
+                    </div>
+                </form>
+                <form class="form-inline" onsubmit="disconnect(); return false;" accept-charset="UTF-8">
+                    <div class="form-group">
                         <button id="disconnect" class="btn btn-default" type="submit" disabled="disabled">Disconnect
                         </button>
                     </div>
                 </form>
             </div>
             <div class="col-md-6">
-                <form class="form-inline">
+                <form class="form-inline" onsubmit="sendChat(); return false;" accept-charset="UTF-8">
                     <div class="form-group">
                         <label for="name">What is your name?</label>
                         <input type="text" id="name" class="form-control" placeholder="Your name here..." />
@@ -21,7 +25,7 @@
                         <label for="message">Input Message</label>
                         <input type="text" id="chatMessage" class="form-control" placeholder="message.." />
                     </div>
-                    <button id="chatSend" class="btn btn-default" type="button">Chat Send</button>
+                    <button id="chatSend" class="btn btn-default" type="submit">Chat Send</button>
                 </form>
             </div>
         </div>
