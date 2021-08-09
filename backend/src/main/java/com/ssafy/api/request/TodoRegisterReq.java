838869d7ed6e15cf57e4dd53f6f0c3ead52afbfc
정범진 <last9500@naver.com>
2021-08-09 16:32:26 +0900

    feat: 오늘의 할일 기능 Back API 구현

---
+++ backend/src/main/java/com/ssafy/api/request/TodoRegisterReq.java
@@ -0,0 +1,23 @@
+package com.ssafy.api.request;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Getter
+@Setter
+@ApiModel("TodoRegisterRequest")
+public class TodoRegisterReq {
+    @ApiModelProperty(name="할일 날짜", example="")
+    LocalDate date;
+    @ApiModelProperty(name="할일 시간", example="")
+    LocalTime time;
+    @ApiModelProperty(name="할일 제목", example="")
+    String title;
+    @ApiModelProperty(name="할일 적용할 부서", example="")
+    Long departmentId;
+}
