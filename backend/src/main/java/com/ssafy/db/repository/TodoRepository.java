572e614d78ce74423813ac00fb712b2f1bfef951
이재영 <giver12@naver.com>
2021-08-12 01:58:09 +0900

    feat: 일자별 todo 상세 조회 API 구현

--- backend/src/main/java/com/ssafy/db/repository/TodoRepository.java
+++ backend/src/main/java/com/ssafy/db/repository/TodoRepository.java
@@ -4,6 +4,10 @@ import com.ssafy.db.entity.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface TodoRepository extends JpaRepository<Todo, Long> {
+    List<Todo> findAllByDateBetween(LocalDate start, LocalDate end);
 }
