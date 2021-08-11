069d4d6e247bc06de82c28545e083eb6eb5d8e59
이재영 <giver12@naver.com>
2021-08-11 15:35:42 +0900

    feat: front 확장자로 검색 탭

--- frontend/src/pages/downloads/DownloadsContent.vue
+++ frontend/src/pages/downloads/DownloadsContent.vue
@@ -5,7 +5,8 @@
           <div class="col-3">
           </div>
           <div class="col-5">
-            <q-input
+            <!-- <input ref="input" /> -->
+            <input style="padding: 20px"
             ref="input"
             outlined v-model="router.currentRoute.value.query.fileName"
             :dense="dense"
