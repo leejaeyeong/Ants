a20213b62b86e489195d354c1d50781140c4a2d4
김지안 <jiahn.dev@gmail.com>
2021-08-12 19:18:28 +0900

    fix: 게시글쓰기 오류, 게시판 화면통일

--- frontend/src/pages/board/BoardMark.vue
+++ frontend/src/pages/board/BoardMark.vue
@@ -1,28 +1,31 @@
 <template>
-  <div id="board">
-    <div>
+  <div id="allBoard">
+    <div id="board">
+      <div>
         <q-input filled color="teal" style="height:40px; display:inline-block; width:20%; margin-top:35px; margin-left:40px;" label="제목, 내용" />
         <q-btn round style="margin-top:-20px; margin-left:10px; background-color: #18C75E; color:white; height:50px; width:50px;" icon="search" />
-    </div>
-    <div class="q-pa-md" style="width:120%; margin-top:30px; margin-left:25px;">
-      <q-table
-        title="공지사항"
-        :rows="rows"
-        :columns="columns"
-        row-key="name"
-        v-model:pagination="pagination"
-        hide-pagination
-      />
-      <div class="row justify-center q-mt-md">
-        <q-pagination
-          v-model="pagination.page"
-          color="grey-8"
-          :max="pagesNumber"
-          size="lg"
+      </div>
+      <div class="q-pa-md" style="width:100%; margin-top:30px; margin-left:25px;">
+        <q-table
+          title="공지사항"
+          :rows="rows"
+          :columns="columns"
+          row-key="name"
+          v-model:pagination="pagination"
+          hide-pagination
         />
+        <div class="row justify-center q-mt-md">
+          <q-pagination
+            v-model="pagination.page"
+            color="grey-8"
+            :max="pagesNumber"
+            size="lg"
+          />
+        </div>
       </div>
     </div>
   </div>
+
 
 </template>
 
@@ -57,13 +60,12 @@ export default defineComponent({
 </script>
 
 <style scoped>
-
 #board{
-    width: 1130px;
-    height: 690px;
-    position: absolute;
-    top:75px;
-    left:390px;
+  width: 1350px;
+  height: 750px;
+  position: absolute;
+  top:30px;
+  left:100px;
 }
 @font-face {
     font-family: 'NEXON Lv1 Gothic OTF';
--- frontend/src/pages/board/BoardWrite.vue
+++ frontend/src/pages/board/BoardWrite.vue
@@ -1,100 +1,166 @@
 <template>
-  <div id="board">
-      <q-select color="teal" filled v-model="form.type" :options="options" style="margin-top:25px; width:15%; margin-left:30px; float:left;" label="분류" />
-      <q-input color="teal" v-model="form.title" style="width:54%; margin-left:15px; margin-top:25px; display:inline-block;" filled label="제목" />
-      <q-btn @click="regist" style="margin-left:10px; padding:10px; margin-top:-20px; color:white; background-color: #00BF6F;" label="등록" />
-      <q-btn @click="back" style="margin-left:10px; padding:10px; margin-top:-20px; color:white;" color="deep-orange" label="뒤로" />
-                  <q-btn @click="imgLabelClick" style="background-color: #00BF6F; margin-left:10px; padding:10px; margin-top:-20px; color:white;">이미지 업로드</q-btn>
-                <div>
-                  <!-- <label for="image_uploads" style="color: black;" @click="imgLabelClick">Choose images to upload (PNG, JPG)</label> -->
-                  <input ref="imageInput" type="file" style="color: black; opacity: 0;" @change="onChangeImages" accept=".jpg, .jpeg, .png" id="profile_img_upload">
-                </div>
-                <img
-                v-if="form.tmp"
-                :src="form.tmp"
-                style="border-radius: 50%; width: 100px; height: 100px;"
-                id="thumb"
-                />
-      <div class="q-pa-md q-gutter-sm">
-        <q-editor v-model="form.content" min-height="32rem" style="margin-left:22px; font-size:30px;"/>
-      </div>
-  </div>
+  <div id="allBoard">
+    <div id="board">
+      <q-select
+        color="teal"
+        outlined
+        v-model="form.type"
+        :options="options"
+        style="margin-top: 25px; width: 15%; margin-left: 30px; float: left"
+        label="분류"
+      />
+      <q-input
+        color="teal"
+        v-model="form.title"
+        outlined
+        
+        style="
+          width: 54%;
+          margin-left: 15px;
+          margin-top: 25px;
+          display: inline-block;
+          color: white;
+        "
+        label="제목"
+      />
 
+      <q-btn
+        @click="imgLabelClick"
+        style="
+          background-color: #00bf6f;
+          margin-left: 10px;
+          padding: 10px;
+          margin-top: -20px;
+          color: white;
+        "
+        >이미지 업로드</q-btn
+      >
+      <div>
+        <!-- <label for="image_uploads" style="color: black;" @click="imgLabelClick">Choose images to upload (PNG, JPG)</label> -->
+        <input
+          ref="imageInput"
+          type="file"
+          style="color: black; opacity: 0"
+          @change="onChangeImages"
+          accept=".jpg, .jpeg, .png"
+          id="profile_img_upload"
+        />
+      </div>
+      <img
+        v-if="form.tmp"
+        :src="form.tmp"
+        style="border-radius: 50%; width: 100px; height: 100px"
+        id="thumb"
+      />
+      <div class="q-pa-md q-gutter-sm">
+        <q-editor
+          v-model="form.content"
+          min-height="32rem"
+          style="margin-left: 22px; font-size: 30px"
+        />
+      </div>
+      <q-btn
+        @click="back"
+        style="float: right; color: white; background-color: rgb(255, 167, 38); margin-right: 10px;"
+        label="뒤로"
+      />
+      <q-btn
+        @click="regist"
+        style="
+          float: right;
+          margin-left: 10px;
+          margin-right: 10px;
+          color: white;
+          background-color: #00bf6f;
+        "
+        label="등록"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
-import { ref, defineComponent, reactive } from 'vue'
-import { useRouter } from 'vue-router'
-import { useStore } from 'vuex'
+import { ref, defineComponent, reactive } from "vue";
+import { useRouter } from "vue-router";
+import { useStore } from "vuex";
 
 export default defineComponent({
-  name: 'write',
-  setup () {
-    const store = useStore()
-    const router = useRouter()
-    const editor = ref('')
-    const Swal = require('sweetalert2')
+  name: "write",
+  setup() {
+    const store = useStore();
+    const router = useRouter();
+    const editor = ref("");
+    const Swal = require("sweetalert2");
     const form = reactive({
-      type: '',
-      title: '',
-      content: '',
+      type: "",
+      title: "",
+      content: "",
       image: null,
-      tmp: ''
-    })
-    const options = [
-        'Google', 'Facebook', 'Twitter', 'Apple', 'Oracle'
-      ]
+      tmp: "",
+      writer: "",
+    });
+    const options = ["익명 게시판", "프론트엔드", "백엔드", "플랫폼", "마케팅"];
 
     const back = function () {
-      router.back()
-      console.log(form.title)
-      console.log(form.content)
-    }
-    function regist () {
-      const frm = new FormData()
-      frm.append('type', 1) //form.type
-      frm.append('writer', localStorage.getItem('id'))
-      frm.append('title', form.title)
-      frm.append('content', form.content)
-      frm.append('image', form.image)
-      store.dispatch('module/registBoard', frm)
+      router.back();
+      console.log(form.title);
+      console.log(form.content);
+    };
+    let rows = [];
+    function regist() {
+      const frm = new FormData();
+      frm.append("type", 1); //form.type
+      frm.append("writer", localStorage.getItem("id"));
+      frm.append("title", form.title);
+      frm.append("content", form.content);
+      frm.append("image", form.image);
+      store
+        .dispatch("module/registBoard", frm)
+        .then(function (result) {
+          console.log(result);
+          store
+            .dispatch("module/board", {})
             .then(function (result) {
+              for (let i = 0; i < result.data.length; i++) {
+                console.log(i + "번쨰 포문");
+                result.data[i].registrationTime = result.data[i].registrationTime.substr(0, 16);
+                rows.push(result.data[i]);
+              }
+              store.commit("module/setRows", rows);
+              rows = [];
+              const pn = Math.ceil(rows.length / 8);
+              store.commit("module/setPageNumber", pn);
               Swal.fire({
-                title: '<span style="font-family:NEXON Lv1 Gothic OTF; font-size:16px;">게시글이 등록되었습니다.</span>',
-                confirmButtonColor: '#19CE60',
-                confirmButtonText: '<span style="font-family:NEXON Lv1 Gothic OTF; font-size:14px;">확인</span>'
-              })
-               store.dispatch('module/board', { })
-                .then(function (result) {
-                  for (let i = 0; i < result.data.length; i++) {
-                    rows.push(result.data[i])
-                  }
-                  store.commit('module/setRows', rows)
-                  rows = []
-                  const pn = Math.ceil(rows.length / 8)
-                  store.commit('module/setPageNumber', pn)
-                  router.push('/board')
-                })
-                .catch(function () {
-                  alert('오류발생')
-                })
+                title:
+                  '<span style="font-family:NEXON Lv1 Gothic OTF; font-size:16px;">게시글이 등록되었습니다.</span>',
+                confirmButtonColor: "#19CE60",
+                confirmButtonText:
+                  '<span style="font-family:NEXON Lv1 Gothic OTF; font-size:14px;">확인</span>',
+              });
+              router.push("/board");
             })
             .catch(function (err) {
-              alert(err)
-            })
+              console.log(err, "에러임");
+              alert("오류발생", err);
+            });
+        })
+        .catch(function (err) {
+          console.log(err);
+          alert("마지막 캐치 오류");
+        });
     }
-    function onChangeImages (e) {
-      console.log(e.target.files)
-      console.log(e.target)
-      const file = e.target.files[0] // Get first index in files
-      this.form.tmp = URL.createObjectURL(file)
-      this.form.image = file // Create File URL
+    function onChangeImages(e) {
+      console.log(e.target.files);
+      console.log(e.target);
+      const file = e.target.files[0]; // Get first index in files
+      this.form.tmp = URL.createObjectURL(file);
+      this.form.image = file; // Create File URL
     }
     // 기존 파일업로드 버튼 숨기고 q-btn으로 대체
-    function imgLabelClick () {
-      const inputImg = document.getElementById('profile_img_upload') // input file 태그 저장
-      console.log(inputImg)
-      inputImg.click() // 클릭이벤트 실행
+    function imgLabelClick() {
+      const inputImg = document.getElementById("profile_img_upload"); // input file 태그 저장
+      console.log(inputImg);
+      inputImg.click(); // 클릭이벤트 실행
     }
 
     return {
@@ -104,33 +170,33 @@ export default defineComponent({
       options,
       imgLabelClick,
       onChangeImages,
-      regist
-    }
-  }
-})
+      regist,
+    };
+  },
+});
 </script>
 
 <style scoped>
-#board{
-    width: 1130px;
-    height: 690px;
-    position: absolute;
-    top:85px;
-    left:390px;
+#board {
+  width: 1350px;
+  height: 750px;
+  position: absolute;
+  top:30px;
+  left:100px;
 }
 @font-face {
-    font-family: 'NEXON Lv1 Gothic OTF';
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-04@2.1/NEXON Lv1 Gothic OTF.woff') format('woff');
-    font-weight: normal;
-    font-style: normal;
+  font-family: "NEXON Lv1 Gothic OTF";
+  src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-04@2.1/NEXON Lv1 Gothic OTF.woff")
+    format("woff");
+  font-weight: normal;
+  font-style: normal;
 }
-div{
-  font-family: 'NEXON Lv1 Gothic OTF';
+div {
+  font-family: "NEXON Lv1 Gothic OTF";
 }
-span{
-  font-family: 'NEXON Lv1 Gothic OTF';
+span {
+  font-family: "NEXON Lv1 Gothic OTF";
 }
 .btn {
-
 }
 </style>
